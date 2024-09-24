b4f9e1f76ebc06b606104c503c82762bfc9db50a
vardhan12178 <balavardhan12178@gmail.com>
2024-09-24 21:17:29 +0530

    Fixes cors issue

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const fs = require('fs');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://instant-video-downloader.vercel.app/'],
-  methods: 'GET,POST',
-  allowedHeaders: 'Content-Type, Authorization'
-}));
-
+    origin: ['http://localhost:3000', 'https://instant-video-downloader.vercel.app'], // No trailing slash
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type, Authorization'
+  }));
+  
 app.use(express.json());
 
 const downloadVideo = (videoUrl, outputFilePath, retries, delay) => {
